bcb6cd80d05157eb4b85081546dba7a3097376fc
Normand Cyr <normand.cyr@gmail.com>
2018-02-13 12:18:11 -0500

    maj gitignore

--- wrapper.sh
+++ wrapper.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # script bash pour tout faire rouler ensemble
 
+# renommer ancien fichier
 date_heure=$(date +%y%m%d_%H%M%S)
-mv donnees_skidefond.json donnees_skidefond.$date_heure.json
+mv /var/www/html/normandcyr.com/apps/data/skidefond/donnees_skidefond.json /var/www/html/normandcyr.com/apps/data/skidefond/donnees_skidefond.$date_heure.json
 
+# aller chercher les conditions actuelles
 ./obtenir_toutes_conditions.py
-./skidefond.py
+
+# copier dans dossier accessible sur le web
+chmod 644 donnees_skidefond.json
+mv donnees_skidefond.json /var/www/html/normandcyr.com/apps/data/skidefond/.
+
+#./skidefond.py
